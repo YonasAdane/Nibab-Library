0e1af324a68b6b6a43df18fb0e444cffd9c740b9
YonasAdane <yonasadane94@gmail.com>
2024-12-14 23:07:32 +0300

    admin books created with sample data

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -19,41 +19,18 @@ import { Hourglass, StickyNote, User, UserRoundPlus, Users } from "lucide-react"
 
 export default function AdminPage() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-          <div className="flex items-center gap-2 px-3">
-            <SidebarTrigger />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
+    
+        <div className="flex flex-1 flex-col gap-4 p-4 ">
           <div className="grid auto-rows-min gap-4 md:grid-cols-4">
             <AdminCard icon={Users} label="Total Visitors" quantity={246}/>
             <AdminCard icon={StickyNote} label="Borrowed Books" quantity={740}/>
             <AdminCard icon={Hourglass} label="Over Due Books" quantity={34}/>
             <AdminCard icon={UserRoundPlus} label="New Members" quantity={60}/>
-            {/* <div className="aspect-video rounded-xl bg-muted/50" /> */}
           </div>
           <div className="min-h-[100vh] w-1/2 flex-1 rounded-xl  md:min-h-min" >
             <BarGraphComponent/>
           </div>
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+      
   )
 }
